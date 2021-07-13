7e5095738c9993f7692d336e3997d81d679b3952
hi-ais <86742603+hi-ais@users.noreply.github.com>
2021-07-13 16:17:32 +0300

    third review.

--- homework.py
+++ homework.py
@@ -97,17 +97,3 @@ class CashCalculator(Calculator):
             message = (f'Денег нет, держись: твой долг - {cash_remained}'
                        f' {name}')
         return message
-cash_calculator = CashCalculator(1000)
-
-# дата в параметрах не указана,
-# так что по умолчанию к записи
-# должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment='кофе'))
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000,
-                                  comment='бар в Танин др',
-                                  date='08.11.2019'))
-
-print(cash_calculator.get_today_cash_remained('rub'))
